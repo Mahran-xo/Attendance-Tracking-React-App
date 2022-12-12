44802cabca98765b04472dc1101d5b304016eb53
Mahran-xo <96589883+Mahran-xo@users.noreply.github.com>
2022-12-12 16:11:54 +0200

    dfgfdgdfgdfgdf

--- src/pages/SubmitAbscenceForm.js
+++ src/pages/SubmitAbscenceForm.js
@@ -1,23 +1,17 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-
-import { data } from 'autoprefixer';
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
-//import FormInputError from '../../UI/form/FormInputError';
 import TextInput from '../UI/form/TextInput';
-// import SubmitAbscenceForm from "../components/students/SubmitAbscenceForm";
 
 const SubmitAbscenceForm = () => {
 
     const [Students, setStudents] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
-    const params = useParams();
     const studentId = "638d0c4fd5fde209c75837fa";
     const module = "638d1d4fc01fc9b4497d2c7c";
     
-    const navigate = useNavigate();
 
     const { register, handleSubmit, formState } = useForm();
 
@@ -46,26 +40,6 @@ const SubmitAbscenceForm = () => {
         }
     };
 
-
-    // useEffect(() => {
-    //     const fetchAbortController = new AbortController();
-    //     const fetchSignal = fetchAbortController.signal;
-
-    //     const fetchStudents = async () => {
-    //         try {
-
-    //     };
-
-    //     fetchStudents();
-
-    //     return () => {
-    //         fetchAbortController.abort();
-    //     };
-    // }, []);
-
-    // if (isLoading) {
-    //     return <p>Loading Form...</p>;
-    // }
 
     return (
         <div>
